d1c37fd4565f3dec320e20a7fb543d0f2f45bec5
Dominik Jancik <hi@domj.net>
2021-02-10 21:18:15 +0200

    +v inset shadows ; +f: initial expressions

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import Squr from './Squr/Squr';
 import { TimeContext } from './Squr/TimeContext';
 import useTime from './useTime';
 
-const SIDE = 300
+const SIDE = '20em'
 
 function App() {
   const time = useTime()
   return (
     <TimeContext.Provider value={time}>
-    <div style={{display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '10px'}}>
-      <Squr side={SIDE}/>
-      <Squr side={SIDE}/>
-      <Squr side={SIDE}/>
-      <Squr side={SIDE}/>
-    </div>
+      {/* TODO Redo with minimal grid based centering */}
+      {/* TODO gif animation credit */}
+      <div style={{display: 'flex', height: '100vh', width: '100vw', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{display: 'grid', gridTemplateColumns: `repeat(2, ${SIDE})`, gap: '2em'}}>
+          <Squr side={SIDE} init="0"/>
+          <Squr side={SIDE} init="1"/>
+          <Squr side={SIDE} init="t%1"/>
+          <Squr side={SIDE} init="sin(t)"/>
+        </div>
+      </div>
     </TimeContext.Provider>
   );
 }
--- src/Squr/Squr.tsx
+++ src/Squr/Squr.tsx
@@ -13,7 +13,7 @@
 
 
 
- 
+
 //useAnimationFrame((time) => {setDimmer(state => (state + time / 1000) % 1)})
 
 // TODO multiple preset animations
@@ -36,6 +36,10 @@ import { TimeContext } from './TimeContext'
 
 interface Props {
     side?: number | string
+    /**
+     * Initial expression
+     */
+    init?: string
 }
 
 const sqr = (side: number | string) => ({
@@ -56,12 +60,12 @@ type ParseError = null | {
     description: string
 }
 
-function Squr({side = 100}: Props): ReactElement {
+function Squr({init, side = 100}: Props): ReactElement {
     const time = useContext(TimeContext)
     const [uptime, setUptime] = useState(0)
     useAnimationFrame((time) => {setUptime(ut => ut + time / 1000)})
 
-    const [expression, setExpression] = useState('.5+t%1')
+    const [expression, setExpression] = useState(init || '0')
     
     const lastValidAst = useRef(parse(''))
     const parseError = useRef<ParseError>(null)
@@ -79,12 +83,15 @@ function Squr({side = 100}: Props): ReactElement {
     return (
         <div style={
             {
-                ...{borderRadius: '0.5em', padding: '1em', boxSizing: 'border-box'},
+                ...{borderRadius: '0.5em', padding: '1em', boxSizing: 'border-box', boxShadow: 'inset 0em -.2em .5em #abc'},
                 ...sqr(side),
                 ...dim(res),
             }
         }>
-            <input style={{fontFamily: 'monospace', borderRadius: '0.3em', padding: '1em', boxSizing: 'border-box', border: 'none', width: '90%'}} type="text" value={expression} onChange={e => setExpression(e.target.value)} />
+            <input
+                style={{fontFamily: 'monospace', borderRadius: '0.3em', padding: '1em', boxSizing: 'border-box', border: 'none', width: '100%', boxShadow: 'inset 0em .2em .5em #abc'}}
+                type="text" value={expression} onChange={e => setExpression(e.target.value)}
+            />
             {parseError.current && <div style={{color: 'red'}}>{parseError.current.description}</div>}
         </div>
     )
