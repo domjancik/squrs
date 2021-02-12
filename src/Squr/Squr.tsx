d36c083c057b54d16fb764fc3039abc3be2b97ea
Dominik Jancik <hi@domj.net>
2021-02-12 18:06:17 +0200

    +f: scale from note array (pentatonic)

--- src/Squr/Squr.tsx
+++ src/Squr/Squr.tsx
@@ -64,11 +64,24 @@ const trns = (a: number) => {
     return { background: `rgba(${COLOR_RGB}, ${a})`}
 }
 
+const black = (a: number) => {
+    return { background: `rgba(0,0,0, ${a})`}
+}
+
 // TODO available in expr eval lib?
 type ParseError = null | {
     index: number
     description: string
 }
+
+const notes = ['C', 'G', 'D', 'A', 'E']
+const getNote = (i: number) => {
+    const note = notes[i % notes.length]
+    const octave = Math.floor(i / notes.length)
+    return `${note}${octave + 2}`
+}
+// const getNote = (i: number) => `C${i}`
+const getNoteFrequency = (i: number) => Tone.Frequency(getNote(i)).toFrequency()
 
 function Squr({init, side = 100, expression: expressionExternal, setExpression: setExpressionExternal, variables = {}}: SqurProps   ): ReactElement {
     const time = useContext(TimeContext)
@@ -99,14 +112,13 @@ function Squr({init, side = 100, expression: expressionExternal, setExpression: 
 
     const fontColor = res < 0.5 ? '#abc' : '#444'
 
-
     // #region AUDIO
 
     // Trigger notes - didn't sound half bad
     const prevRes = useRef(res)
     const synth = useRef<Tone.Synth<Tone.SynthOptions> | null>(null)
     useEffect(() => {
-        if (res - prevRes.current > 0.5) synth.current?.triggerAttackRelease(`C${variables.i}`, "8n")
+        if (res - prevRes.current > 0.95) synth.current?.triggerAttackRelease(getNote(variables.i), "8n")
         prevRes.current = res
     }, [res])
 
@@ -120,9 +132,10 @@ function Squr({init, side = 100, expression: expressionExternal, setExpression: 
     const osc = useRef<Tone.Oscillator | null>(null)
     useEffect(() => {
         osc.current = new Tone.Oscillator({
-            frequency: 32 * Math.pow(2, variables.i),
+            // frequency: 32 * Math.pow(2, variables.i),
+            frequency: getNoteFrequency(variables.i),
             type: "sawtooth4",
-            volume: -20,
+            volume: -30,
             detune: Math.random() * 30 - 15,
         }).toDestination().start()
         return () => {
@@ -132,24 +145,27 @@ function Squr({init, side = 100, expression: expressionExternal, setExpression: 
 
     useEffect(() => {
         if (isNaN(res)) return
-        const volumeTarget = res === 0 ? -Infinity : lerp(-80, -40, res)
+        const volumeTarget = res === 0 ? -Infinity : lerp(-80, -20, res)
         osc.current?.volume.rampTo(volumeTarget, 0.01)
     }, [res])
 
     // #endregion
+
+    const color = res > 0 ? trns(res) : black(-res)
 
     return (
         <div style={
             {
                 ...{borderRadius: '0.5em', padding: '0.5em', boxSizing: 'border-box', boxShadow: 'inset 0em -.2em .5em #abc'},
                 ...sqr(side),
-                ...trns(res),
+                ...color,
             }
         }>
             <textarea
-                style={{fontFamily: 'monospace', borderRadius: '0.3em', padding: '0.5em', boxSizing: 'border-box', border: 'none', width: '100%', boxShadow: 'inset 0em .2em .5em #abc', background: 'transparent', color: fontColor, fontWeight: 'bold', transition: 'all 300ms', maxWidth: '100%', minWidth: '100%', minHeight: '2em', maxHeight: '100%'}}
+                style={{fontFamily: 'monospace', borderRadius: '0.3em', padding: '0.5em', boxSizing: 'border-box', border: 'none', width: '100%', boxShadow: 'inset 0em .2em .5em #abc', background: 'transparent', color: fontColor, fontWeight: 'bold', transition: 'all 300ms', maxWidth: '100%', minWidth: '100%', minHeight: '50%', maxHeight: '100%'}}
                 value={expression} onChange={e => setExpression(e.target.value)}
             />
+            {getNote(variables.i)}
             {parseError.current && <div style={{color: 'red'}}>{parseError.current.description}</div>}
         </div>
     )
