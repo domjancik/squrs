e8a1a96227e4a7ceb341a2cb8ecff103a79db15b
Dominik Jancik <hi@domj.net>
2022-04-02 22:07:12 +0300

    gridseq on local squr

--- src/Squr/SqurLocalStorage.tsx
+++ src/Squr/SqurLocalStorage.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react'
 import useLocalStorageState from '../useLocalStorageState'
 import Squr from './Squr'
 import SqurProps from './SqurProps'
+import { INSTRUMENTS } from "./instruments";
 
 interface SqurLocalStorageProps
   extends Omit<
@@ -14,8 +15,13 @@ interface SqurLocalStorageProps
 function SqurLocalStorage({storageKey, ...props}: SqurLocalStorageProps): ReactElement {
     const [expression, setExpression] = useLocalStorageState(storageKey, 'sin(t)')
 
+    const { logic, view } = INSTRUMENTS["gridseq"];
     return (
-        <Squr expression={expression} setExpression={setExpression} {...props} />
+        <Squr expression={expression} setExpression={setExpression}
+        
+      contentComponent={view}
+      useExpressionHook={logic}
+        {...props} />
     )
 }
 
