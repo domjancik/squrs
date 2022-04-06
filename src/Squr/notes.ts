c493a7ef4c562d3f2ed556e343b4fa2a56e4cb6b
Dominik Jancik <hi@domj.net>
2022-04-06 09:44:13 +0300

    cleanup, format

--- src/Squr/notes.ts
+++ src/Squr/notes.ts
@@ -1,77 +1,77 @@
-import * as Tone from 'tone'
+import * as Tone from "tone";
 
 interface ScaleWindow extends Window {
-    sqursSetScale: (index: number) => void
+  sqursSetScale: (index: number) => void;
 }
 
-declare let window: ScaleWindow 
+declare let window: ScaleWindow;
 
-// const notes = ['C', 'G', 'D', 'A', 'E']
-// const notes = ['C', 'D', 'E', 'A', 'G']
+const C = "C";
+const D = "D";
+const E = "E";
+const Eb = "Eb";
+const F = "F";
+const G = "G";
+const A = "A";
+const Ab = "Ab";
+const B = "B";
+const Bb = "Bb";
+const Db = "Db";
+const Gb = "Gb";
 
-const C = 'C'
-const D = 'D'
-const E = 'E'
-const Eb = 'Eb'
-const F = 'F'
-const G = 'G'
-const A = 'A'
-const Ab = 'Ab'
-const B = 'B'
-const Bb = 'Bb'
-
-const SCALE_5_MAJOR_PENTATONIC = [C, D, E, G, A]
-const SCALE_5_EGYPTIAN_SUSPENDED = [C, D, F, G, Bb]
-const SCALE_5_BLUES_MINOR_MAN_GONG = [C, Eb, F, Ab, Bb]
-const SCALE_5_BLUES_MAJOR_RITSUSEN = [C, D, F, G, A]
-const SCALE_5_MINOR_PENTATONIC = [C, Eb, F, G, Bb]
-const SCALE_4_TONEJS_SEQUENCER_EXAMPLE = [A, 'Db', E, 'Gb']
+const SCALE_5_MAJOR_PENTATONIC = [C, D, E, G, A];
+const SCALE_5_EGYPTIAN_SUSPENDED = [C, D, F, G, Bb];
+const SCALE_5_BLUES_MINOR_MAN_GONG = [C, Eb, F, Ab, Bb];
+const SCALE_5_BLUES_MAJOR_RITSUSEN = [C, D, F, G, A];
+const SCALE_5_MINOR_PENTATONIC = [C, Eb, F, G, Bb];
+const SCALE_4_TONEJS_SEQUENCER_EXAMPLE = [A, Db, E, Gb];
 
 type ScaleItem = {
-    title: string,
-    notes: string[]
-}
+  title: string;
+  notes: string[];
+};
 const SCALES: ScaleItem[] = [
-    {
-        title: "Major Pentatonic (5)",
-        notes: SCALE_5_MAJOR_PENTATONIC
-    },
-    {
-        title: "Egyptian Suspended (5)",
-        notes:SCALE_5_EGYPTIAN_SUSPENDED 
-    },
-    {
-        title: "Blues Major Ritsusen (5)",
-        notes: SCALE_5_BLUES_MAJOR_RITSUSEN 
-    },
-    {
-        title: "Blues Minor Man Gong (5)",
-        notes: SCALE_5_BLUES_MINOR_MAN_GONG 
-    },
-    {
-        title: "Blues Minor Pentatonic (5)",
-        notes: SCALE_5_MINOR_PENTATONIC
-    },
-    {
-        title: "ToneJS Sequencer Example (4)",
-        notes: SCALE_4_TONEJS_SEQUENCER_EXAMPLE 
-    },
-]
+  {
+    title: "Major Pentatonic (5)",
+    notes: SCALE_5_MAJOR_PENTATONIC,
+  },
+  {
+    title: "Egyptian Suspended (5)",
+    notes: SCALE_5_EGYPTIAN_SUSPENDED,
+  },
+  {
+    title: "Blues Major Ritsusen (5)",
+    notes: SCALE_5_BLUES_MAJOR_RITSUSEN,
+  },
+  {
+    title: "Blues Minor Man Gong (5)",
+    notes: SCALE_5_BLUES_MINOR_MAN_GONG,
+  },
+  {
+    title: "Blues Minor Pentatonic (5)",
+    notes: SCALE_5_MINOR_PENTATONIC,
+  },
+  {
+    title: "ToneJS Sequencer Example (4)",
+    notes: SCALE_4_TONEJS_SEQUENCER_EXAMPLE,
+  },
+];
 
-let notes = SCALE_5_BLUES_MAJOR_RITSUSEN 
+let notes = SCALE_5_BLUES_MAJOR_RITSUSEN;
 
 window.sqursSetScale = (index: number) => {
-    const scale = SCALES[index % SCALES.length]
-    console.log(`Changing to scale ${scale.title}`)
-    notes = scale.notes
-}
+  const scale = SCALES[index % SCALES.length];
+  console.log(`Changing to scale ${scale.title}`);
+  notes = scale.notes;
+};
 
 const getNote = (i: number) => {
-    const note = notes[i % notes.length]
-    const octave = Math.floor(i / notes.length)
-    return `${note}${octave + 3}`
-}
-// const getNote = (i: number) => `C${i}`
-const getNoteFrequency = (i: number) => Tone.Frequency(getNote(i)).toFrequency()
+  const note = notes[i % notes.length];
+  const octave = Math.floor(i / notes.length);
+  return `${note}${octave + 3}`;
+};
 
-export { getNote, getNoteFrequency }
+const getNoteFrequency = (i: number) =>
+  Tone.Frequency(getNote(i)).toFrequency();
+
+export { getNote, getNoteFrequency };
--- src/Squr/useAnimationFrame.ts
+++ src/Squr/useAnimationFrame.ts
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const useAnimationFrame = (callback: (time: number) => void) => {
   // Use useRef for mutable variables that we want to persist
